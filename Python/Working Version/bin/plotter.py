a01b0f36e8677da4e96e9c2910fe1c0799879ee5
Claudio Vestini <cvestini@gmail.com>
2026-09-16 00:01:23 -0400

    rename legacy plotter constraints to A and b

--- Python/Working Version/bin/plotter.py
+++ Python/Working Version/bin/plotter.py
@@ -3,11 +3,11 @@ This module provides functions for visualising the intersection of
 half-spaces in 1D and 2D.
 
 Functions:
-- plot_2d_space(N, c, X, Y, label, cmap, ax):
+- plot_2d_space(A, b, X, Y, label, cmap, ax):
     Plots a 2D region defined by the intersection of half-spaces.
-- plot_1d_space(N, c, label, cmap, ax):
+- plot_1d_space(A, b, label, cmap, ax):
     Plots a 1D region (line) defined by the intersection of half-spaces.
-- plot_half_spaces(Nc_pairs, num_of_iterations, ax):
+- plot_half_spaces(ab_pairs, num_of_iterations, ax):
     Plots the intersection of multiple sets of half-spaces.
 - plot_path(path, ax, errors_for_plotting=None, plot_errors=False):
     Plots the path followed by Dykstra's algorithm during projection,
@@ -30,14 +30,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_2d_space(N: np.ndarray, c: np.ndarray, X: np.ndarray, Y: np.ndarray,
+def plot_2d_space(A: np.ndarray, b: np.ndarray, X: np.ndarray, Y: np.ndarray,
                   label: str, cmap: str, ax) -> None:
     """
     Plots a 2D region defined by the intersection of half spaces.
 
     Args:
-        N: Matrix of normal vectors.
-        c: Vector of constant offsets.
+        A: Matrix of normal vectors.
+        b: Vector of constant offsets.
         X: 2D array of x coordinates.
         Y: 2D array of y coordinates.
         label: Label for the plot.
@@ -52,17 +52,17 @@ def plot_2d_space(N: np.ndarray, c: np.ndarray, X: np.ndarray, Y: np.ndarray,
     Z = np.ones_like(X)
 
     # This loop checks whether a pair of points X,Y lies within the set of constraints
-    # Iterate over each half-plane (one per row of N)
-    for i in range(N.shape[0]):
+    # Iterate over each half-plane (one per row of A)
+    for i in range(A.shape[0]):
         # Calculate the dot product and compare with the offset
         # - X.ravel() and Y.ravel() flatten the 2D arrays X and Y into 1D arrays;
         # - np.vstack([...]) stacks the flattened X and Y arrays vertically,
         #   creating a 2D array where each row represents a point (x, y) from the grid.
         # - .T transposes the resulting array, so each column now represents a point.
-        dot_product = np.dot(np.vstack([X.ravel(), Y.ravel()]).T, N[i])
+        dot_product = np.dot(np.vstack([X.ravel(), Y.ravel()]).T, A[i])
         # Z will contain 1s for points inside the intersection of all
         # half-planes and 0s for points outside
-        Z = np.where(dot_product.reshape(X.shape) > c[i], 0, Z)  # Reshape before comparison
+        Z = np.where(dot_product.reshape(X.shape) > b[i], 0, Z)  # Reshape before comparison
 
     # Colours
     colourmap = plt.get_cmap(cmap)
@@ -76,14 +76,14 @@ def plot_2d_space(N: np.ndarray, c: np.ndarray, X: np.ndarray, Y: np.ndarray,
     ax.plot([], [], color=colour, alpha=0.5, label=label)
 
 
-def plot_1d_space(N: np.ndarray, c: np.ndarray, label: str, cmap: str, ax,
+def plot_1d_space(A: np.ndarray, b: np.ndarray, label: str, cmap: str, ax,
                   x_range: list)-> None:
     """
     Plots a 1D region (line) defined by the intersection of half spaces.
 
     Args:
-        N: Matrix of normal vectors.
-        c: Vector of constant offsets.
+        A: Matrix of normal vectors.
+        b: Vector of constant offsets.
         label: Label for the plot.
         cmap: Colourmap for the plot.
         ax: Axes handle for plotting.
@@ -97,30 +97,30 @@ def plot_1d_space(N: np.ndarray, c: np.ndarray, label: str, cmap: str, ax,
     colour = colourmap(0.69) # again, a total coincidence
 
     # Check for division by 0
-    if N[0, 1] == 0:
+    if A[0, 1] == 0:
         # Vertical line
-        ax.axvline(x=c[0] / N[0, 0], linestyle='-', linewidth=2,
+        ax.axvline(x=b[0] / A[0, 0], linestyle='-', linewidth=2,
                     label='Vertical Line', color=colour)
-    elif N[0, 0] == 0:
+    elif A[0, 0] == 0:
         # Horizontal line
-        ax.axhline(y=c[0] / N[0, 1], linestyle='-', linewidth=2,
+        ax.axhline(y=b[0] / A[0, 1], linestyle='-', linewidth=2,
                     label='Horizontal Line', color=colour)
     else:
         # General line
         x_line = np.linspace(x_range[0], x_range[1], 100)  # Adjust range as needed
-        y_line = (c[0] - N[0, 0] * x_line) / N[0, 1]
+        y_line = (b[0] - A[0, 0] * x_line) / A[0, 1]
         ax.plot(x_line, y_line, linewidth=2, label=label, color=colour)
 
 
-def plot_half_spaces(Nc_pairs: list, num_of_iterations: int, ax,
+def plot_half_spaces(ab_pairs: list, num_of_iterations: int, ax,
                      x_range: list, y_range: list) -> None:
     """
-    Plots the intersection of multiple sets of half-spaces defined by Nc_pairs,
-    where each pair consists of N (normals) and c (offsets) such that N*x <= c.
-    Nc_pairs is of the form [('label'_i, 'cmap_i', N_i, c_i), (...), ...]
+    Plots the intersection of multiple sets of half-spaces defined by ab_pairs,
+    where each pair consists of A (normals) and b (offsets) such that A*x <= b.
+    ab_pairs is of the form [('label'_i, 'cmap_i', A_i, b_i), (...), ...]
 
     Args:
-        Nc_pairs: List of tuples containing ('label', 'cmap', N, c).
+        ab_pairs: List of tuples containing ('label', 'cmap', A, b).
         num_of_iterations: Number of iterations.
         ax: Axes handle for plotting.
 
@@ -135,19 +135,19 @@ def plot_half_spaces(Nc_pairs: list, num_of_iterations: int, ax,
         y = np.linspace(y_range[0], y_range[1], 500)
         X, Y = np.meshgrid(x, y)
 
-        for label, cmap, N, c in Nc_pairs:
-            # Distinguish cases based on the rank of N
-            rank = np.linalg.matrix_rank(N)
+        for label, cmap, A, b in ab_pairs:
+            # Distinguish cases based on the rank of A
+            rank = np.linalg.matrix_rank(A)
             # 1d case (line)
             if rank == 1:
-                plot_1d_space(N, c, label, cmap, ax, x_range)
+                plot_1d_space(A, b, label, cmap, ax, x_range)
             # 2d case
             elif rank == 2:
-                plot_2d_space(N, c, X, Y, label, cmap, ax)
+                plot_2d_space(A, b, X, Y, label, cmap, ax)
             # Everything else (ADD 3D CASE AT SOME POINT)
             else:
                 raise ValueError("Dimension not supported."
-                                 "Please provide N and c for 1D or 2D cases.")
+                                 "Please provide A and b for 1D or 2D cases.")
 
         # Set aspect ratio to 'equal'
         ax.set_aspect('equal')
@@ -164,10 +164,10 @@ def plot_half_spaces(Nc_pairs: list, num_of_iterations: int, ax,
 
     except TypeError as e:
         print(f"TypeError occurred: {e}."
-              f"Please ensure Nc_pairs is a list of tuples.")
+              f"Please ensure ab_pairs is a list of tuples.")
     except ValueError as e:
         print(f"ValueError occurred: {e}."
-              f"Check the format of Nc_pairs or the dimensions of N.")
+              f"Check the format of ab_pairs or the dimensions of A.")
 
 
 def plot_path(path: list, ax, errors_for_plotting=None,
